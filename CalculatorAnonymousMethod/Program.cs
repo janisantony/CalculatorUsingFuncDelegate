b1b036044eb8976de50eca841adad40ac9ed44cb
janisantony <120107676+janisantony@users.noreply.github.com>
2022-12-09 09:01:44 +0400

    Add files via upload

---
+++ CalculatorAnonymousMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CalculatorAnonymousMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double,double,double> anonymousAdd = delegate (double firstNumber, double secondNumber)
+            {
+                return firstNumber + secondNumber;
+            };
+
+            Func<double, double, double> anonymousSubtract = delegate (double firstNumber, double secondNumber)
+            {
+                return firstNumber - secondNumber;
+            };
+
+            Func<double, double, double> anonymousMultiply = delegate (double firstNumber, double secondNumber)
+            {
+                return firstNumber * secondNumber;
+            };
+
+            Func<double, double, double> anonymousDivide = delegate (double firstNumber, double secondNumber)
+            {
+                return firstNumber / secondNumber;
+            };
+
+            Calculator(anonymousAdd);
+            Calculator(anonymousSubtract);
+            Calculator(anonymousMultiply);
+            Calculator(anonymousDivide);
+        }
+        private static void Calculator(Func<double, double, double> calcFunction)
+        {
+
+            Console.WriteLine(calcFunction(20, 40));
+        }
+    }
+}
