8f14bc7591ba8b2a30a7f17f14295993e2c125a8
janisantony <120107676+janisantony@users.noreply.github.com>
2022-12-09 08:56:35 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,56 @@
 ﻿using System;
 
-namespace CalculatorUsingFuncDelegate
+namespace CheckNumbersEqual
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Func<double, double, double> addDelegate = new Func<double, double, double>(AddMethod);
-            Console.WriteLine("Add :" +addDelegate(20, 30));
-
-            Func<double, double, double> subtractDelegate = new Func<double, double, double>(SubtractMethod);
-            Console.WriteLine("Subtract :" + subtractDelegate(20, 30));
-
-            Func<double, double, double> multiplyDelegate = new Func<double, double, double>(MultiplyMethod);
-            Console.WriteLine("Multiplly :" + multiplyDelegate(20, 30));
-
-            Func<double, double, double> divideDelegate = new Func<double, double, double>(DivideMethod);
-            Console.WriteLine("Divide :" + divideDelegate(20, 30));
-
+            EnterNumbersForComparison();
         }
 
-
-        private static double AddMethod(double firstNumber, double secNumber)
+        private static void EnterNumbersForComparison()
         {
-            return firstNumber + secNumber;
+            int firstNumber;
+            int secNumber;
+            bool numberCheck = false;
+            while (!numberCheck)
+            {
+                Console.WriteLine("Enter the first number");
+                if (int.TryParse(Console.ReadLine(), out firstNumber))
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Enter the second number");
+                        if (int.TryParse(Console.ReadLine(), out secNumber))
+                        {
+                            CheckNumbersAreEqual(firstNumber, secNumber);
+                            numberCheck = true;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid entry");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid entry");
+                }
+            }
         }
 
-        private static double SubtractMethod(double firstNumber, double secNumber)
+        private static void CheckNumbersAreEqual(int firstNumber, int secNumber)
         {
-            return firstNumber - secNumber;
-        }
-
-        private static double MultiplyMethod(double firstNumber, double secNumber)
-        {
-            return firstNumber * secNumber;
-        }
-
-        private static double DivideMethod(double firstNumber, double secNumber)
-        {
-            return firstNumber / secNumber;
+            if (firstNumber == secNumber)
+            {
+                Console.WriteLine("Both numbers are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both numbers are not equal");
+            }
         }
     }
 }
