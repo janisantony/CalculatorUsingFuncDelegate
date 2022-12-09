2882ccac670870ef89145fb5b264f4b494e9a5c0
janisantony <120107676+janisantony@users.noreply.github.com>
2022-12-09 09:00:29 +0400

    Add files via upload

---
+++ DelegateChaining/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DelegateChaining
+{
+    class Program
+    {
+        public delegate void CalculatorDelegate(double firstNumber, double secNumber);
+        static void Main(string[] args)
+        {
+            var addDelegate = new CalculatorDelegate(AddMethod);
+            addDelegate += SubtractMethod;
+            addDelegate += MultiplyMethod;
+            addDelegate += DivideMethod;
+            addDelegate(30, 40);
+             
+          
+        }
+
+        private static void AddMethod(double firstNumber, double secNumber)
+        {
+            Console.WriteLine(firstNumber + secNumber);
+        }
+
+        private static void SubtractMethod(double firstNumber, double secNumber)
+        {
+            Console.WriteLine(firstNumber - secNumber);
+
+        }
+
+        private static void MultiplyMethod(double firstNumber, double secNumber)
+        {
+            Console.WriteLine(firstNumber * secNumber);
+
+        }
+        private static void DivideMethod(double firstNumber, double secNumber)
+        {
+            Console.WriteLine(firstNumber / secNumber);
+
+        }
+    }
+}
